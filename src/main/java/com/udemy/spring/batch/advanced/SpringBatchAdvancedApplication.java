0c9bfa695d9a2c97f66962f93f2b72ea5f737052
felipe107896 <felipe.massa10@hotmail.com>
2020-09-27 10:46:27 -0300

    primeiro commit

---
+++ src/main/java/com/udemy/spring/batch/advanced/SpringBatchAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.batch.advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchAdvancedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/spring/batch/advanced/SpringBatchAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.batch.advanced;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchAdvancedApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
